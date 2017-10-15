6e89a0e2d166dc95d42f6b911facb0c740f39183
Jeff Brown Tech <Jeff@JeffBrown.tech>
2017-10-15 14:04:04 -0500

    Added connecting and searching to each CsServer

--- FixSipAddress.ps1
+++ FixSipAddress.ps1
@@ -52,5 +52,55 @@ BEGIN
 
 PROCESS
 {
-    
+    foreach ($sip in $SipAddress)
+    {
+        foreach ($csServer in $allCsServers)
+        {
+            # Attempt to create SQL Connection
+            try
+            {
+                $sqlConn = New-Object System.Data.SqlClient.SqlConnection -ErrorAction STOP
+                $sqlConn.ConnectionString = "Server=$csServer\rtclocal;Integrated Security=true;Initial Catalog=master"
+                $sqlConn.Open()
+            }
+            catch
+            {
+                $connErrorMsg = $_.Exception.Message
+                
+                if ($connErrorMsg -like "*The server was not found or was not accessible*")
+                {
+                    Write-Warning -Message "$csServer was not found or is not accessible. Verify firewall rules allow UDP 1434 and SQLServr.exe as exceptions."
+                }
+                else
+                {
+                    Write-Warning -Message $connErrorMsg
+                }
+
+                CONTINUE
+            }
+
+            # Create SQL Query
+            $sqlCmd = New-Object System.Data.SqlClient.SqlCommand
+            $sqlCmd.Connection = $sqlConn
+            $searchQuery = "SELECT * FROM [rtc].[dbo].[Resource] WHERE [UserAtHost]='$sip'"
+            $sqlCmd.CommandText = $searchQuery
+
+            $adapter = New-Object System.Data.SqlClient.SqlDataAdapter $sqlCmd
+            $searchResults = New-Object System.Data.DataSet
+            $adapter.Fill($searchResults) | Out-Null
+
+            if ($null -ne $searchResults.Tables.UserAtHost)
+            {
+                # Found user
+                # Remove user
+            }
+            else
+            {
+                # Some message saying user not found
+            }
+
+            # Close SQL Connection
+            $sqlConn.Close()
+        } # End of foreach ($csServer in $allCsServers)
+    } # End of foreach ($sip in $SipAddress)
 } # End of PROCESS
